662d524c665d7f7029be49edc83e6c58b839c075
Jason Strese <jason@strese.io>
2024-05-11 21:26:10 -0500

    feat: support for selfhosted spacelift (#5)

--- .spacelift/test/test.tf
+++ .spacelift/test/test.tf
@@ -11,10 +11,16 @@ provider "spacelift" {}
 
 variable "spacelift_run_id" {}
 
+variable "spacelift_url" {
+  type    = string
+  default = "app.spacelift.io"
+}
+
 module "msteams-integration" {
   source = "../../"
 
   channel_name = var.spacelift_run_id
   space_id     = "public-modules-01GVNH2CJKSKHRSMDPBMQ3WZT9"
   webhook_url  = "https://devnull-as-a-service.com/dev/null"
+  spacelift_url = var.spacelift_url
 }
--- policy.tf
+++ policy.tf
@@ -3,6 +3,13 @@ resource "spacelift_policy" "msteams-integration" {
   type     = "NOTIFICATION"
   space_id = var.space_id
 
-  body   = file("${path.module}/assets/policy.rego")
+  body   = local.is_custom_spacelift_url ? local.custom_policy : local.default_policy
   labels = ["msteams"]
+}
+
+locals {
+  default_policy = file("${path.module}/assets/policy.rego")
+  custom_policy  = templatefile("${path.module}/assets/policy-custom.rego", {
+    spacelift_url = var.spacelift_url
+  })
 }
